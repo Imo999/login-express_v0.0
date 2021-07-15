6655f0b27770c706e2465535c2f71076cd0d4ad9
Imo999 <rcajina.pro@gmail.com>
2021-07-15 13:17:35 +0200

    add router / pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,18 @@
+import { Switch, Route, Redirect } from 'react-router-dom'
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-      INTERFACE APP
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Redirect to='/' />
+      </Switch>
     </div>
   );
 }
---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      Interface Login
+    </div>
+  )
+}
+
+export default Login
