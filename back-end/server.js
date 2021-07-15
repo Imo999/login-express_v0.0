5229310f909ee83d9f7500f1fc35f48b43377b51
Imo999 <rcajina.pro@gmail.com>
2021-07-15 14:10:52 +0200

    update

--- back-end/server.js
+++ back-end/server.js
@@ -4,10 +4,25 @@ const app = express();
 // process.env.PORT pour la production // 4000 pour le DEV
 const PORT = process.env.PORT || 4000;
 
+// DOUTE TODOOOOO 
+app.set("view engine", 'react');
 
+// Route vers les pages 
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.render('Home')
 });
+
+app.get('/users/register', (req, res) => {
+  res.render('Register')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('Login')
+})
+
+app.get('/users/dashboard', (req, res) => {
+  res.render('Dashboard')
+})
 
 
 app.listen(PORT, () => {
