8a6363a60ffb0726ddabb9830e5ac54d59023c1c
Imo999 <rcajina.pro@gmail.com>
2021-07-15 21:14:00 +0200

    Update front

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,37 @@
-import { Switch, Route, Redirect } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/users/login' component={Login} />
-      <Route exact path='/users/register' component={Register} />
-      <Route exact path='/users/dashboard' component={Dashboard} />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
 
-    </Switch>
-  );
+  callAPI() {
+    fetch("http://localhost:4000/")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/users/login' component={Login} />
+        <Route exact path='/users/register' component={Register} />
+        <Route exact path='/users/dashboard' component={Dashboard} />
+      </Switch>
+    );
+  }
 }
 
 export default App;
-
-// <Redirect to='/' />
--- front-end/src/pages/Dashboard.js
+++ front-end/src/pages/Dashboard.js
@@ -4,7 +4,7 @@ const Dashboard = () => {
   return (
     <div>
       <a href="/users/logout" > Logout </a>
-      <h1>Hello ${ } vous êtes sur votre Dashboard</h1>
+      <h1>Hello vous êtes sur votre Dashboard</h1>
     </div>
   )
 }
