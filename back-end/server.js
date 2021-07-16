35e4192423e6885b07a4168412d4a1bcf271edd4
Imo999 <rcajina.pro@gmail.com>
2021-07-16 07:07:31 +0200

    Update main

--- back-end/server.js
+++ back-end/server.js
@@ -1,19 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const { cb_loggerAccess, timerAccess } = require('./controllers/accessLogControllers');
 const { cb_loggerRegister } = require('./controllers/registerLogControllers');
 const { cb_loggerLogin } = require('./controllers/loginLogControllers');
 const { cb_loggerDashboard } = require('./controllers/dashboardLogControllers');
+const { checkerUser, checkerPassword } = require('./controllers/checkAuthentification');
+
 const { home } = require('./routes/home');
 const { register } = require('./routes/Register');
 const { login } = require('./routes/login');
 const { dashboard } = require('./routes/dashboard');
+const fs = require('fs');
 
 const app = express();
+
+
+app.use(cors());
+// Configure express to use body-parser as middleware.
+app.use(express.urlencoded({ extended: false })); // to support URL-encoded bodies
+app.use(express.json()); // to support JSON-encoded bodies
+
 
 // ##### Middleware #####
 app.use(timerAccess, cb_loggerAccess); // Log sauvegardé -> "access-log.txt"
 app.use('/users/register', cb_loggerRegister); // Log sauvegardé -> "register-log.txt"
-app.use('/users/login', cb_loggerLogin); // Log sauvegardé -> "login-log.txt"
+app.use('/users/login', cb_loggerLogin); // Log sauvegardé -> "login-log.txt" 
+app.use('/users/login', checkerUser); // Vérification User
+app.use('/users/login', checkerPassword); // Vérification Password
 app.use('/users/dashboard', cb_loggerDashboard); // Log sauvegardé -> "dashboard-log.txt"
 
 // ##### Routing #####
@@ -21,6 +34,17 @@ app.use('/', home);
 app.use('/users/register', register);
 app.use('/users/login', login);
 app.use('/users/dashboard', dashboard);
+
+// TEST
+/*
+let rawdata = fs.readFileSync('./config/dbLocal.json');
+let dbLocal = JSON.parse(rawdata);
+console.log(dbLocal);
+
+let jsonData = require('./config/dbLocal.json');
+console.log(jsonData);
+*/
+
 
 // start the server
 const IP = '192.168.1.13' // my local ip on my network
